1b7a0d0bab4ed88942e6bf2c6f111e1685fe71b4
Razgriz85 <elchicoconinsomnio@gmail.com>
2015-07-12 02:42:10 +0200

    First Test scene

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour 
+{
+
+	public GameObject m_PlayerCamera;
+	private Camera m_PlayerCameraComponent;
+	private Camera m_CameraComponent;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_CameraComponent = gameObject.GetComponent<Camera>();
+		m_PlayerCameraComponent = m_PlayerCamera.GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void OnTravelFinished ()
+	{
+		m_PlayerCameraComponent.enabled = true;
+		m_CameraComponent.enabled = false;
+	}
+}
